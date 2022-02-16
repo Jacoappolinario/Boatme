cb2a544a969207f880f92ec18852603f2c538ae8
Jacó Apolinário <jacoapollinario@gmail.com>
2022-02-16 08:40:39 -0300

    add avatar user

--- src/shared/infra/typeorm/migrations/1645008418248-AlterUserAddAvatar.ts
+++ src/shared/infra/typeorm/migrations/1645008418248-AlterUserAddAvatar.ts
@@ -1,17 +1,18 @@
 import { MigrationInterface, QueryRunner, TableColumn } from 'typeorm';
 
-export class AlterUserAddPhone1644873438787 implements MigrationInterface {
+export class AlterUserAddAvatar1645008418248 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.addColumn(
       'users',
       new TableColumn({
-        name: 'phone',
+        name: 'avatar',
         type: 'varchar',
+        isNullable: true,
       }),
     );
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.dropColumn('users', 'phone');
+    await queryRunner.dropColumn('users', 'avatar');
   }
 }
